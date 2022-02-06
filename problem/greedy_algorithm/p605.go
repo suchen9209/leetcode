414c883502e128c5c6f8d57a070ac9d357b0c98f
such <suchen9209@126.com>
2022-02-07 00:05:48 +0800

    1

--- problem/greedy_algorithm/p605.go
+++ problem/greedy_algorithm/p605.go
@@ -3,24 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	flowerbed := []int{1,0,0,0,1}
+	flowerbed := []int{1, 0, 0, 0, 1}
 	n := 1
-	fmt.Println(canPlaceFlowers(flowerbed,n))
+	fmt.Println(canPlaceFlowers(flowerbed, n))
 }
 
 func canPlaceFlowers(flowerbed []int, n int) bool {
 	//r := flowerbed[:]
 	r := append(flowerbed, 0)
 	r = append(r, 1)
-	r = append([]int{1,0}, r...)
+	r = append([]int{0, 1}, r...)
 	fmt.Println(r)
 
-	left,canSet := 0,0
+	left, canSet := 0, 0
 
 	for i := 1; i < len(r); i++ {
 		if r[i] == 1 {
 			tmpCanSetNumber := (i - left - 2) / 2
-			if tmpCanSetNumber > 0{
+			if tmpCanSetNumber > 0 {
 				canSet += tmpCanSetNumber
 			}
 			left = i
